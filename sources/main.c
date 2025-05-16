ee7fa21648cd83dbff954299af4fa25a011c4729
Anttoni Maula <manttoni92@gmail.com>
2025-05-16 15:31:27 +0300

    logfile fix

--- sources/main.c
+++ sources/main.c
@@ -16,7 +16,9 @@ WINDOW *leg_win = NULL;
 
 int main(void)
 {
-	fclose((fopen("logs/debug.log", "w")));
+	time_t t = time(NULL);
+	struct tm tm = *localtime(&t);
+	logger("\n%d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 	signal(SIGSEGV, handle_segfault);
 	srand(time(NULL));
 	init_ncurses();
